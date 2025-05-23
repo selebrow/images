758319a61b7e9ac74b325b9b2555ff74723e34cb
selebrow-dev <selebrow.dev@gmail.com>
2025-05-23 17:03:19 +0400

    remove gh configure git

--- scripts/commit-meta.sh
+++ scripts/commit-meta.sh
@@ -4,9 +4,6 @@ NEW_BRANCH="update-images-${RUN_ID}"
 COMMIT_MESSAGE="Auto-update images"
 DESCRIPTION="Chrome ${LATEST_CHROME_VERSION:-???}, Firefox ${LATEST_FIREFOX_VERSION:-???}, Playwright ${LATEST_PLAYWRIGHT_VERSION:-???}"
 
-gh auth login --with-token
-gh auth setup-git
-
 git config user.name "selebrow-ci"
 git config user.email "213042858+selebrow-ci[bot]@users.noreply.github.com"
 
@@ -15,4 +12,5 @@ git add meta.json
 git commit -m "$COMMIT_MESSAGE" -m "$DESCRIPTION" >> /dev/null
 git push -u origin $(git rev-parse --abbrev-ref HEAD) >> /dev/null
 
+gh auth login --with-token
 gh pr create --base main --head "${NEW_BRANCH}" --fill
