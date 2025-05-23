dfeeca99d66db543dcdc7f153d8d7456f098752e
selebrow-dev <selebrow.dev@gmail.com>
2025-05-23 18:15:51 +0400

    setup-git

--- scripts/commit-meta.sh
+++ scripts/commit-meta.sh
@@ -4,6 +4,9 @@ NEW_BRANCH="update-images-${RUN_ID}"
 COMMIT_MESSAGE="Auto-update images"
 DESCRIPTION="Chrome ${LATEST_CHROME_VERSION:-???}, Firefox ${LATEST_FIREFOX_VERSION:-???}, Playwright ${LATEST_PLAYWRIGHT_VERSION:-???}"
 
+gh auth login --with-token
+gh auth setup-git
+
 git config user.name "selebrow-ci[bot]"
 git config user.email "213042858+selebrow-ci[bot]@users.noreply.github.com"
 
@@ -12,5 +15,4 @@ git add meta.json
 git commit -m "$COMMIT_MESSAGE" -m "$DESCRIPTION" >> /dev/null
 git push -u origin $(git rev-parse --abbrev-ref HEAD) >> /dev/null
 
-gh auth login --with-token
 gh pr create --base main --head "${NEW_BRANCH}" --fill
